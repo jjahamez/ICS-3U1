d328c40f327fea5caa92fa2080abe8a2a502f1cf
unknown <342908977@TDSB.ON.CA>
2025-03-24 14:43:50 -0400

    edge case added

--- assignment 5 polynomial evaluator/src/PolynomialEvaluator.java
+++ assignment 5 polynomial evaluator/src/PolynomialEvaluator.java
@@ -4,17 +4,25 @@ import java.util.StringTokenizer;
 public class PolynomialEvaluator {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("Please enter a polynomial in standard f(x) notation");
-        String polynomial = input.nextLine();
+        double sum = 0.0;
 
-        System.out.println("Please enter an x value for the polynomial");
-        int x = input.nextInt();
+        System.out.println("Please enter a polynomial in standard f(x) notation | eg: [f(x)=9x^6 +1x^1 -5x^2 +2...]");
+        String poly = input.nextLine().substring(5); // ignores f(x)= (first 5 characters)
+        System.out.println("Please enter the x value");
+        double x = input.nextDouble();
+        input.close();
 
-        StringTokenizer st = new StringTokenizer(polynomial);
+        StringTokenizer st = new StringTokenizer(poly, " "); // separates terms
         while (st.hasMoreTokens()) {
-            
-            System.out.println(st.nextToken());
+            String term = st.nextToken();  
+            if (term.contains("x")) {
+                double coefficient = Double.parseDouble(term.substring(0, term.indexOf('x')));
+                int exponent = Integer.parseInt(term.substring(term.indexOf('^') + 1)); // determines if before x or after ^
+                sum += coefficient * Math.pow(x, exponent); // if term has "x"
+            } else {
+                sum += Double.parseDouble(term); // constants
+            }
         }
+        System.out.println("f(" + x + ")= " + sum);
     }
 }
