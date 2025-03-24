0a7bd58a18ca6344a88e13c6d44873b76ba0a7ff
jjahamez <stephenchenstephenchen1211@gmail.com>
2025-03-24 02:37:01 -0400

    Update PolynomialEvaluator.java

--- assignment 5 polynomial evaluator/src/PolynomialEvaluator.java
+++ assignment 5 polynomial evaluator/src/PolynomialEvaluator.java
@@ -6,7 +6,7 @@ public class PolynomialEvaluator {
         Scanner input = new Scanner(System.in);
         double sum = 0.0;
 
-        System.out.println("Please enter a polynomial in standard f(x) notation | eg: [f(x)=9x^6 +3.1x^1 +2...]");
+        System.out.println("Please enter a polynomial in standard f(x) notation | eg: [f(x)=9x^6 +1x^1 -5x^2 +2...]");
         String poly = input.nextLine().substring(5); // ignores f(x)= (first 5 characters)
         System.out.println("Please enter the x value");
         double x = input.nextDouble();
